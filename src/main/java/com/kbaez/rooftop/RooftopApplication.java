d6fe493654bbfd0ed05b5974e0631f0925ee2276
tika00 <b.karina.baez@gmail.com>
2022-06-21 18:09:40 -0300

    initial commit

---
+++ src/main/java/com/kbaez/rooftop/RooftopApplication.java
@@ -0,0 +1,13 @@
+package com.kbaez.rooftop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RooftopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RooftopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbaez/rooftop/RooftopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbaez.rooftop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RooftopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
