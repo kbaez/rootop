9f85b1f43cf8f31d5d494b88b97f36ce3f282ab4
tika00 <b.karina.baez@gmail.com>
2022-06-27 23:40:42 -0300

    add tests

--- src/test/java/com/kbaez/rooftop/service/RooftopServiceTest.java
+++ src/test/java/com/kbaez/rooftop/service/RooftopServiceTest.java
@@ -3,17 +3,16 @@ package com.kbaez.rooftop.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.web.client.RestTemplate;
 
+import com.kbaez.rooftop.dto.BlocksResponse;
 import com.kbaez.rooftop.dto.CheckBlockResponse;
 import com.kbaez.rooftop.dto.CheckResponse;
+import com.kbaez.rooftop.dto.TokenResponse;
 import com.kbaez.rooftop.dto.request.BlocksRequest;
 import com.kbaez.rooftop.service.impl.RooftopServiceImpl;
 
@@ -55,5 +54,35 @@ public class RooftopServiceTest {
                .thenReturn(checkBlockResponse);
 		CheckResponse response = rooftopService.check(token, blocksRequest);
 		assertEquals(expectedCheckResponse, response);
+	}
+	
+	@Test
+	public void testGetTokenReturnTokenResponse(){
+		TokenResponse tokenResponse = new TokenResponse();
+		tokenResponse.setToken("e9371ae9-fde2-4c85-ab40-6cef5391586d");
+		Mockito.when(restTemplate.getForObject(Mockito.any(String.class), Mockito.eq(TokenResponse.class))).thenReturn(tokenResponse);
+		TokenResponse response = rooftopService.getToken("mail@gmail.com");
+		assertEquals(tokenResponse, response);
+	}
+	
+	@Test
+	public void testGetBlocksReturnBlocksResponse(){
+		BlocksResponse blocksResponse = new BlocksResponse();
+		blocksResponse.setData(new String[] {
+		                                       "F0gUU2TQPe9fg023U1vPFcwna7IQQyr362RNenYOa2ReTty1aoIh1mYHXB2jGkW825VBEQl2Xkr06AGiQguJbMysx2pWDcVtSdE8",
+		                                       "KokzhmizqlWCXocJO1eXAuMW9JYx0Ba0HSW4TKnKtzI1chHoYQVXAKrF8go87g2c1CU1jjiRLiAi2hQs7IBn7huLokLKqFpZvFS4",
+		                                       "SjkZvFcLV77eOWiuL4zkyJsdjgRGAu6dyyrVBkcmISKv0YJXWmQiJNnEzKzIcWnkW5yaKiCJ0SenSrYPuRQY2TE7Y6p5KpNugIBk",
+		                                       "yJUg341qdmeEFuZqQCEFjfaM0zSVCt5mEliIdpjY0mBaiqqbojjy8J9BKiG9cPBFNfe8fRfttBemXnCmj4wXbj8Of04dGNwd5xER",
+		                                       "Wzsb03mUqttlQaywXiAL5v7ouRH963Ctq42AxExB7TN4DCQb1z8VzZBHOJJPR82RB3Hu04fMu12gYIrQ6jCLjQAhK2QsrOl4ONsD",
+		                                       "Zc8CzGBAFX1Gi24tCXipDRIFR0HtDc9KwxV2Pezwz2dHiZmqNbwNCPHDoY8CUUUhsG7JBOGru1UvkYNJATOs4JozuSi6BzVT0MdN",
+		                                       "UMd39X88TsokdFbHymURR2wm8nnkMqHVT17N7PrnLE2zo9ofRXYzF5xtiruD1cTVdRZsIYSw7QKoH8iq5PncoROjUbS9TFgQqZKC",
+		                                       "Nc3FGEQNHoEBYakpuw91DiWdnIhUvC3OiokkU2eoprarpoLnkp4aEGaz6jCBUl2ql4DrYTqJ2H8HSoU8bMTFJtMxovJIl5t95CIQ",
+		                                       "xJzqcjGFNR8sUspOzw7KMOgmidaZ1oqgBDKl6HwbzukCKHj1cJmxcCuDexTZQhozGJiR7W1ZM1tE6tbFXzSKzabYjpv2BFz0vm1q"
+		                                   });
+		blocksResponse.setChunkSize("100");
+		blocksResponse.setLength("900");
+		Mockito.when(restTemplate.getForObject(Mockito.any(String.class), Mockito.eq(BlocksResponse.class))).thenReturn(blocksResponse);
+		BlocksResponse response = rooftopService.getBlocks("e9371ae9-fde2-4c85-ab40-6cef5391586d");
+		assertEquals(blocksResponse, response);
 	}
 }
